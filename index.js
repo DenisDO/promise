69a7272ac04a9e8227b2ffaba72d0cb1f5cc218e
Denis Derkach <podstavka128@gmail.com>
2019-01-09 17:58:35 +0200

    first

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log('argv[0]', process.argv[0]);
+console.log('argv[1]', process.argv[1]);
+console.log('argv[2]', process.argv[2]);
+console.log('argv[3]', process.argv[3]);
+console.log('argv[4]', process.argv[4]);
